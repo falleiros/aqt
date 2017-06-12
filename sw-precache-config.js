34e68f65dd4b066448bdc989fc6fbc9cb4f6ee5e
falleiros <natalli.falleiros@ifms.edu.br>
2017-06-12 16:19:54 -0400

    markers personalizados

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -15,6 +15,30 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/*',
+    '/images/*'
   ],
-  navigateFallback: 'index.html',
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+  runtimeCaching: [
+    {
+      urlPattern: /\/data\/images\/.*/,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/.*json/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
 };
